4203ab87d9fbc59e2d93e239cc0878f7f7e9d05e
stephane71 <stephanemaire71@gmail.com>
2020-10-13 16:05:58 +0200

    Freelance page | edit content

--- pages/stephane.maire.js
+++ pages/stephane.maire.js
@@ -1,3 +1,28 @@
 export default function Freelance() {
-  return <div>Freelance</div>;
+  return (
+    <div>
+      <h1>Stéphane Maire</h1>
+      <h2>Freelance Développeur Front End</h2>
+      <p>React, NextJS, Material UI, REST API, PWA</p>
+      <p>Bonjour et bienvenue sur cette petite page de présentation.</p>
+      <p>Je m'appelle Stéphane Maire. J'habite à Moroges en Saône et Loire.</p>
+      <p>
+        Je suis développeur front end et spécialisé sur les technologies React.
+        Je développe des applications depuis plus de 10 ans et les technologies
+        web sont devenues une passion. L'écosystème autour de React (Material UI
+        et Next.js en particulier) constitue pour moi les meilleurs technologies
+        du moment et permettent d'avoir un cycle de développement maitrisé
+        (conception - réalisation - maintenance) pour aller au plus vite de
+        l'idée au produit. Au cours de mes développement, je prête attention à
+        la fois à la performances ainsi qu'a la maintenance et la scalabilité
+        d'un produit.
+      </p>
+      <p>
+        Je travail à distance depuis Moroges et j'ai accès rapidement à 2 gares
+        SNCF pour de venir à Paris, Lyon ou Marseille en peu de temps.
+      </p>
+      <div>Lien vers LinkedIn et GitHub</div>
+      <div>Coordonnées: Mail & Téléphone</div>
+    </div>
+  );
 }
