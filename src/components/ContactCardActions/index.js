32cc3de923313ac5cdb824d8541c088497a31473
stephane71 <stephanemaire71@gmail.com>
2020-10-23 13:47:00 +0200

    ContractCardActions | add href to GitHub & LinkedIn

--- src/components/ContactCardActions/index.js
+++ src/components/ContactCardActions/index.js
@@ -4,6 +4,7 @@ import Button from "@material-ui/core/Button";
 import GitHubIcon from "@material-ui/icons/GitHub";
 import LinkedInIcon from "@material-ui/icons/LinkedIn";
 import GetAppIcon from "@material-ui/icons/GetApp";
+import {GITHUB_ACCOUNT_URL, LINKEDIN_PROFIL_URL} from "../../constants";
 import styles from "./styles";
 
 const useStyles = makeStyles(styles);
@@ -14,10 +15,15 @@ function ContactCardActions() {
   return (
     <div className={classes.ContactCardActions}>
       <div>
-        <Button size="small" className={classes.gitHubButton}>
+        <Button
+          size="small"
+          className={classes.gitHubButton}
+          href={GITHUB_ACCOUNT_URL}
+          target="_blank"
+        >
           <GitHubIcon /> &nbsp; Réalisations
         </Button>
-        <Button>
+        <Button size="small" href={LINKEDIN_PROFIL_URL} target="_blank">
           <LinkedInIcon /> &nbsp; Expérience
         </Button>
       </div>
