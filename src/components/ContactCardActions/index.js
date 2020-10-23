8249e83979e324e55bb66d49fc46e1587df93e80
stephane71 <stephanemaire71@gmail.com>
2020-10-23 14:35:04 +0200

    ContactCardActions | add download link to .vcf card ContactCardActions | add Tooltip to download contact ContactCardActions | move icons in startIcon prop of Button

--- src/components/ContactCardActions/index.js
+++ src/components/ContactCardActions/index.js
@@ -1,10 +1,11 @@
 import { makeStyles } from "@material-ui/core/styles";
 import IconButton from "@material-ui/core/IconButton";
 import Button from "@material-ui/core/Button";
+import Tooltip from "@material-ui/core/Tooltip";
 import GitHubIcon from "@material-ui/icons/GitHub";
 import LinkedInIcon from "@material-ui/icons/LinkedIn";
 import GetAppIcon from "@material-ui/icons/GetApp";
-import {GITHUB_ACCOUNT_URL, LINKEDIN_PROFIL_URL} from "../../constants";
+import { GITHUB_ACCOUNT_URL, LINKEDIN_PROFIL_URL } from "../../constants";
 import styles from "./styles";
 
 const useStyles = makeStyles(styles);
@@ -20,16 +21,24 @@ function ContactCardActions() {
           className={classes.gitHubButton}
           href={GITHUB_ACCOUNT_URL}
           target="_blank"
+          startIcon={<GitHubIcon />}
         >
-          <GitHubIcon /> &nbsp; Réalisations
+          Réalisations
         </Button>
-        <Button size="small" href={LINKEDIN_PROFIL_URL} target="_blank">
-          <LinkedInIcon /> &nbsp; Expérience
+        <Button
+          size="small"
+          href={LINKEDIN_PROFIL_URL}
+          target="_blank"
+          startIcon={<LinkedInIcon />}
+        >
+          Expérience
         </Button>
       </div>
-      <IconButton size="small" edge="end">
-        <GetAppIcon />
-      </IconButton>
+      <Tooltip title="Télécharger mon contact">
+        <IconButton size="small" edge="end" href="./Stephane_Maire.vcf">
+          <GetAppIcon />
+        </IconButton>
+      </Tooltip>
     </div>
   );
 }
