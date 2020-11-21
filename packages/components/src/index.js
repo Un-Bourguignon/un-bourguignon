fce75718692a277e62d4177cf552e4f6843dd54e
stephane71 <stephanemaire71@gmail.com>
2020-11-21 13:11:30 +0100

    Components | index: export AppLayout && HeaderSource

--- packages/components/src/index.js
+++ packages/components/src/index.js
@@ -1,2 +1,4 @@
 export { default as ContactCard } from "./ContactCard";
 export { default as useSize } from "./useSize";
+export { HeaderSource } from "./AppLayout";
+export { default as AppLayout } from "./AppLayout";
