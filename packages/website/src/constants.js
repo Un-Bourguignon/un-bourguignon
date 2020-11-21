e8678fab005a119a52545b283a940b98174d3b35
stephane71 <stephanemaire71@gmail.com>
2020-11-21 14:12:11 +0100

    UB Website | constants: add GITHUB_PROFIL_PHOTO & HOME_ADDRESS_GOOGLE_MAPS UB Website | constants: remove LINKEDIN_PROFIL_URL

--- packages/website/src/constants.js
+++ packages/website/src/constants.js
@@ -1,3 +1,3 @@
 export const GITHUB_ACCOUNT_URL = "https://github.com/stephane71";
-export const LINKEDIN_PROFIL_URL =
-  "https://www.linkedin.com/in/st%C3%A9phane-maire-124b7a32";
+export const GITHUB_PROFIL_PHOTO = "https://github.com/stephane71.png";
+export const HOME_ADDRESS_GOOGLE_MAPS = "https://goo.gl/maps/K94VPqUTTuLUd38J8";
