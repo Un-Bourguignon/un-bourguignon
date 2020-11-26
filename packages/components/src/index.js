fd21b2d97312149e157d795f6c56556d1ddd3808
stephane71 <stephanemaire71@gmail.com>
2020-11-26 11:33:50 +0100

    Components | index: export ContactDetails

--- packages/components/src/index.js
+++ packages/components/src/index.js
@@ -1,5 +1,10 @@
 export { default as ContactCard } from "./ContactCard";
+
 export { default as ContactCardActions } from "./ContactCardActions";
+
 export { default as useSize } from "./useSize";
+
 export { HeaderSource } from "./AppLayout";
 export { default as AppLayout } from "./AppLayout";
+
+export { default as ContactDetails } from "./ContactDetails";
