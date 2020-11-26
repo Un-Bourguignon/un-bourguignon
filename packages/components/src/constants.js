3699ba3dbc170d1a5bd14b078bd18923abe0785a
stephane71 <stephanemaire71@gmail.com>
2020-11-26 11:31:39 +0100

    Components | constants: add MOCK_USER

--- packages/components/src/constants.js
+++ packages/components/src/constants.js
@@ -4,3 +4,15 @@ export const LINKEDIN_PROFIL_URL =
 export const CONTACT_FILE_VCF =
   "https://assets.la-foulee.com/Stephane_Maire.vcf";
 export const HOME_ADDRESS_GOOGLE_MAPS = "https://goo.gl/maps/K94VPqUTTuLUd38J8";
+export const MOCK_USER = {
+    image: "https://github.com/stephane71.png",
+    imageTitle: "GitHub profil photo",
+    title: "Stéphane Maire",
+    job: "Développer Front-End",
+    location: {
+        label: "Moroges, France",
+        mapLink: HOME_ADDRESS_GOOGLE_MAPS,
+    },
+    email: "stephane@un-bourguignon.com",
+    phone: "+33 6 62 46 16 43",
+};
