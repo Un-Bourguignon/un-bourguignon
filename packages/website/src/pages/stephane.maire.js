f9ea96ec5c2c99c71ee8c4c1e1e968135445b8ad
stephane71 <stephanemaire71@gmail.com>
2020-11-26 16:40:53 +0100

    UB Website | Freelance page: update page with JSX content instead of mdx

--- packages/website/src/pages/stephane.maire.js
+++ packages/website/src/pages/stephane.maire.js
@@ -1,35 +1,84 @@
 import { makeStyles } from "@material-ui/core/styles";
-import { MDXProvider } from "@mdx-js/react";
+import { ContactDetails, AvatarEnhance } from "@un-bourguignon/components";
 import Container from "@material-ui/core/Container";
-import Freelance from "../markdown/freelance.mdx";
-import Components from "../components/mdx/components";
+import Typography from "@material-ui/core/Typography";
+import Paper from "@material-ui/core/Paper";
+import Button from "@material-ui/core/Button";
+import GetAppIcon from "@material-ui/icons/GetApp";
+import ButtonNetworkProfils from "../components/ButtonsNetworkProfils";
+import { PERSONAL_DETAILS } from "../constants";
 
 const useStyles = makeStyles((theme) => ({
-  wrapper: {
-    height: "100%",
-    paddingBottom: theme.spacing(4),
+  FreelancePage: {
+    position: "relative",
+    paddingBottom: theme.spacing(5),
+  },
+  header: {
+    padding: `${theme.spacing(4)}px 0`,
+  },
+  main: {
+    padding: `${theme.spacing(2)}px 0`,
+  },
+  presentation: {
+    marginBottom: theme.spacing(5),
+  },
+  presentationText: {
+    fontWeight: theme.typography.fontWeightRegular,
+    marginBottom: theme.spacing(1),
+  },
+  buttonDLContact: {
+    marginTop: theme.spacing(1),
+    textTransform: "none",
   },
 }));
 
-function Wrapper({ children }) {
+const FreelancePage = () => {
   const classes = useStyles();
 
   return (
-    <Container maxWidth="sm" className={classes.wrapper} component="section">
-      {children}
+    <Container
+      maxWidth="sm"
+      component="section"
+      className={classes.FreelancePage}
+    >
+      <header className={classes.header}>
+        <AvatarEnhance img={PERSONAL_DETAILS.image} externalLinks={[]} />
+      </header>
+
+      <main className={classes.main}>
+        <div className={classes.presentation}>
+          <Typography
+            variant="h6"
+            component="p"
+            color="textSecondary"
+            className={classes.presentationText}
+          >
+            Je suis développeur Front-End et travaille à distance depuis la
+            Saône-et-Loire. J'ai accès rapidement à Paris, Lyon et Marseille
+            pour nous rencontrer.
+          </Typography>
+          <ButtonNetworkProfils />
+        </div>
+
+        <Paper variant="outlined">
+          <ContactDetails
+            email={PERSONAL_DETAILS.email}
+            location={PERSONAL_DETAILS.location}
+            phone={PERSONAL_DETAILS.phone}
+          />
+        </Paper>
+
+        <Button
+          startIcon={<GetAppIcon />}
+          size="large"
+          variant="outlined"
+          className={classes.buttonDLContact}
+        >
+          Télécharger mon contact
+        </Button>
+      </main>
     </Container>
   );
-}
-
-const components = {
-  ...Components,
-  wrapper: Wrapper,
 };
-
-const FreelancePage = () => (
-  <MDXProvider components={components}>
-    <Freelance />
-  </MDXProvider>
-);
 
 export default FreelancePage;
