53f5ecfabbd9902409ed6941fbae92cf1b7c2c28
stephane71 <stephanemaire71@gmail.com>
2020-11-26 16:37:37 +0100

    UB Website | ContactCardStephaneMaire: import data from constants

--- packages/website/src/components/ContactCardStephaneMaire/index.js
+++ packages/website/src/components/ContactCardStephaneMaire/index.js
@@ -1,21 +1,14 @@
-import { ContactCard } from "@un-bourguignon/components";
-import { GITHUB_PROFIL_PHOTO, HOME_ADDRESS_GOOGLE_MAPS } from "../../constants";
-
-const USER = {
-  image: GITHUB_PROFIL_PHOTO,
-  imageTitle: "GitHub profil photo",
-  title: "Stéphane Maire",
-  job: "Développer Front-End",
-  location: {
-    label: "Moroges, France",
-    mapLink: HOME_ADDRESS_GOOGLE_MAPS,
-  },
-  email: "stephane@un-bourguignon.com",
-  phone: "+33 6 62 46 16 43",
-};
+import { ContactCard, ContactCardActions } from "@un-bourguignon/components";
+import { PERSONAL_DETAILS } from "../../constants";
 
 function ContactCardStephaneMaire(props) {
-  return <ContactCard {...USER} {...props} />;
+  return (
+    <ContactCard
+      {...PERSONAL_DETAILS}
+      actions={<ContactCardActions />}
+      {...props}
+    />
+  );
 }
 
 export default ContactCardStephaneMaire;
