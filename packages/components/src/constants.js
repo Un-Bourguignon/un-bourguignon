e8f0288e758664cc532b49eaccca01e700897d9a
stephane71 <stephanemaire71@gmail.com>
2020-11-26 13:45:00 +0100

    Components | constants: replace mock data with json file from UB website

--- packages/components/src/constants.js
+++ packages/components/src/constants.js
@@ -1,18 +1,3 @@
-export const GITHUB_ACCOUNT_URL = "https://github.com/stephane71";
-export const LINKEDIN_PROFIL_URL =
-  "https://www.linkedin.com/in/st%C3%A9phane-maire-124b7a32";
-export const CONTACT_FILE_VCF =
-  "https://assets.la-foulee.com/Stephane_Maire.vcf";
-export const HOME_ADDRESS_GOOGLE_MAPS = "https://goo.gl/maps/K94VPqUTTuLUd38J8";
-export const MOCK_USER = {
-    image: "https://github.com/stephane71.png",
-    imageTitle: "GitHub profil photo",
-    title: "Stéphane Maire",
-    job: "Développer Front-End",
-    location: {
-        label: "Moroges, France",
-        mapLink: HOME_ADDRESS_GOOGLE_MAPS,
-    },
-    email: "stephane@un-bourguignon.com",
-    phone: "+33 6 62 46 16 43",
-};
+import PersonalContactDetails from "../../website/public/Stephane_Maire.json";
+
+export const MOCK_USER = PersonalContactDetails;
