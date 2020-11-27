783dad89861939dc434824f64f1e75b6bc8f20c7
stephane71 <stephanemaire71@gmail.com>
2020-11-27 18:58:14 +0100

    Components | AvatarEnhance: update html tag to header 1 & 2

--- packages/components/src/AvatarEnhance/index.js
+++ packages/components/src/AvatarEnhance/index.js
@@ -18,14 +18,14 @@ function AvatarEnhance({ firstname, lastname, img, externalLinks = [] }) {
         className={classes.avatar}
       />
       <div className={classes.text}>
-        <Typography variant="h6" component="p" className={classes.name}>
+        <Typography variant="h6" component="h1" className={classes.name}>
           {firstname} <span style={{ opacity: 0.7 }}>{lastname}</span>
         </Typography>
 
         <Typography
           variant="subtitle1"
           color="textSecondary"
-          component="p"
+          component="h2"
           className={classes.subtitle}
         >
           Développeur Front-End
