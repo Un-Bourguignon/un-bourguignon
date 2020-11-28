8e6816f2b2c008fbcc1a0d6fb229c40382c15411
stephane71 <stephanemaire71@gmail.com>
2020-11-28 14:25:46 +0100

    Components | AvatarEnhance: update stories

--- packages/components/src/AvatarEnhance/stories.js
+++ packages/components/src/AvatarEnhance/stories.js
@@ -1,4 +1,5 @@
 import React from "react";
+import IconButton from "@material-ui/core/IconButton";
 import GitHubIcon from "@material-ui/icons/GitHub";
 import LinkedInIcon from "@material-ui/icons/LinkedIn";
 import { MOCK_USER } from "../constants";
@@ -19,14 +20,29 @@ const externalLinks = [
     href: MOCK_USER.GitHub,
   },
 ];
+const socialNetworkLinks = (
+  <div>
+    {externalLinks.map(({ icon: Icon, href }, i) => (
+      <IconButton
+        key={href}
+        edge={i === 0 ? "start" : false}
+        size="small"
+        style={{ marginRight: 8 }}
+        href={href}
+      >
+        <Icon fontSize="small" />
+      </IconButton>
+    ))}
+  </div>
+);
 
 const Template = (props) => <AvatarEnhance img={MOCK_USER.image} {...props} />;
 
 export const Default = Template.bind({});
 Default.args = {
-  externalLinks,
   firstname: MOCK_USER.firstname,
   lastname: MOCK_USER.lastname,
+  bottomComponent: socialNetworkLinks,
 };
 
 export const WithoutExternalLinks = Template.bind({});
