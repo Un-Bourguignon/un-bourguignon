00cd62c8274dbc4819cd9766d1f059590698f9d9
stephane71 <stephanemaire71@gmail.com>
2020-11-28 18:06:07 +0100

    UB Website | Home page: update layout and render

--- packages/website/src/pages/index.js
+++ packages/website/src/pages/index.js
@@ -1,21 +1,79 @@
 import Link from "next/link";
-import { HeaderSource } from "@un-bourguignon/components";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
+import Typography from "@material-ui/core/Typography";
+import { AvatarEnhance } from "@un-bourguignon/components";
+import { PERSONAL_DETAILS } from "../constants";
+import ButtonBase from "@material-ui/core/ButtonBase";
+
+const useStyles = makeStyles((theme) => ({
+  Home: {
+    height: "100%",
+    width: "100%",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  content: {
+    padding: `0 ${theme.spacing(2)}px`,
+  },
+  headerFontSize: {
+    fontSize: theme.typography.h3.fontSize,
+  },
+  pages: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "stretch",
+    marginTop: theme.spacing(7),
+  },
+  marginBottom: {
+    marginBottom: theme.spacing(3),
+  },
+  buttonPage: {
+    padding: theme.spacing(2),
+    "&:hover": {
+      backgroundColor: theme.palette.action.hover,
+    },
+  },
+}));
 
 function Home() {
+  const classes = useStyles();
+  const theme = useTheme();
+  const matches = useMediaQuery(theme.breakpoints.down("xs"));
+
   return (
-    <div>
-      <HeaderSource>
-        <h1>Un Bourguignon</h1>
-      </HeaderSource>
+    <div className={classes.Home}>
+      <div className={classes.content}>
+        <Typography
+          variant="h2"
+          component="h1"
+          className={matches ? classes.headerFontSize : ""}
+          paragraph
+        >
+          Un Bourguignon
+        </Typography>
 
-      <p>Service de développement web && éditeur logiciel</p>
+        <div className={classes.pages}>
+          <ButtonBase className={classes.buttonPage}>
+            <Typography variant="h6" component="p" color="textSecondary">
+              🛠️ Edition logiciel
+            </Typography>
+          </ButtonBase>
 
-      <div>
-        <ul>
-          <li>
-            <Link href="/stephane.maire">Freelance</Link>
-          </li>
-        </ul>
+          <Link href="/stephane.maire" passHref>
+            <ButtonBase className={classes.buttonPage} component="a">
+              <AvatarEnhance
+                img={PERSONAL_DETAILS.image}
+                firstname="Freelance"
+                lastname=""
+                job={PERSONAL_DETAILS.job}
+                small
+                asParagraph
+              />
+            </ButtonBase>
+          </Link>
+        </div>
       </div>
     </div>
   );
