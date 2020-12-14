742811a98db4956fd6f81d283afaebb92a67491d
stephane71 <stephanemaire71@gmail.com>
2020-12-14 11:38:02 +0100

    UB Website | AppFooter: Wrap company name with a Link to go home

--- packages/website/src/components/AppFooter/index.js
+++ packages/website/src/components/AppFooter/index.js
@@ -1,6 +1,8 @@
+import Link from "next/link";
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
 import Typography from "@material-ui/core/Typography";
+import LinkMui from "@material-ui/core/Link";
 import styles from "./styles";
 
 const useStyles = makeStyles(styles);
@@ -11,14 +13,18 @@ function AppFooter() {
   return (
     <div className={classes.AppFooter}>
       <Container maxWidth="sm" className={classes.container}>
-        <Typography
-          variant="body1"
-          paragraph
-          className={classes.footerAppName}
-          color="textPrimary"
-        >
-          Un Bourguignon
-        </Typography>
+        <Link href="/" passHref>
+          <LinkMui color="inherit">
+            <Typography
+              variant="body1"
+              paragraph
+              className={classes.footerAppName}
+              color="textPrimary"
+            >
+              Un Bourguignon
+            </Typography>
+          </LinkMui>
+        </Link>
         <Typography variant="body2" color="textSecondary">
           Copyright © 2020 Un Bourguignon
         </Typography>
