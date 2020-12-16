59f255c912699adeba92a638e1a90769924a8d6a
stephane71 <stephanemaire71@gmail.com>
2020-12-16 16:47:55 +0100

    UB Website | AvatarPhoto: create component

---
+++ packages/website/src/components/AvatarPhoto/index.js
@@ -0,0 +1,24 @@
+import Image from "next/image";
+import { makeStyles } from "@material-ui/core/styles";
+import { AVATAR_DIMENSION } from "@un-bourguignon/components";
+import { PERSONAL_DETAILS } from "../../constants";
+import styles from "./styles";
+
+const useStyles = makeStyles(styles);
+
+function AvatarPhoto({ small }) {
+  const classes = useStyles();
+
+  return (
+    <Image
+      src={PERSONAL_DETAILS.image}
+      alt={`${PERSONAL_DETAILS.firstname} ${PERSONAL_DETAILS.lastname}`}
+      width={small ? AVATAR_DIMENSION.small : AVATAR_DIMENSION.default}
+      height={small ? AVATAR_DIMENSION.small : AVATAR_DIMENSION.default}
+      layout="fixed"
+      className={classes.AvatarPhoto}
+    />
+  );
+}
+
+export default AvatarPhoto;
---
+++ packages/website/src/components/AvatarPhoto/styles.js
@@ -0,0 +1,8 @@
+const styles = (theme) => ({
+  AvatarPhoto: {
+    border: `1px solid ${theme.palette.divider} !important`,
+    borderRadius: "50%",
+  },
+});
+
+export default styles;
