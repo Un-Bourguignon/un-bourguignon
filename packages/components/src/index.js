668ad6294be7ccca92f8eff4da317d7cae489d35
stephane71 <stephanemaire71@gmail.com>
2020-12-18 18:02:49 +0100

    UB Components | index: remove HeaderSource export

--- packages/components/src/index.js
+++ packages/components/src/index.js
@@ -4,7 +4,6 @@ export { default as ContactCardActions } from "./ContactCardActions";
 
 export { default as useSize } from "./useSize";
 
-export { HeaderSource } from "./AppLayout";
 export { default as AppLayout } from "./AppLayout";
 
 export { default as ContactDetails } from "./ContactDetails";
